4a5bfd216a8a3a1b4c03f21a4ec59a0f217be9d6
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2024-01-09 18:36:43 +0100

    hacer primeros tests

--- PruebaTest1/src/test/java/org/example/AppTest.java
+++ PruebaTest1/src/test/java/org/example/AppTest.java
@@ -1,13 +1,13 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
 
@@ -20,7 +20,41 @@ public class AppTest {
     void test1(){
         HashSet<Integer> hs = new HashSet<>(List.of(1,2,3,4,5));
         Collection<Integer> c = Collections.emptyList();
+        assertFalse(hs.removeAll(c));
+    }
 
+    @Test
+    void test2(){
+        HashSet<Integer> hs = new HashSet<>(List.of(1,2,3,4,5));
+        Collection<Integer> c = Collections.unmodifiableCollection(List.of(1));
+        assertTrue(hs.removeAll(c));
+    }
+
+    @Test
+    void test3(){
+        HashSet<Integer> hs = new HashSet<>(List.of(1,2,3,4,5));
+        Collection<Integer> c = Collections.unmodifiableCollection(List.of(1,2,3,4,5));
+        assertTrue(hs.removeAll(c));
+    }
+
+    @Test
+    void test4(){
+        HashSet<Integer> hs = new HashSet<>(Collections.emptyList());
+        Collection<Integer> c = Collections.unmodifiableCollection(List.of(1,2,3,4,5));
+        assertFalse(hs.removeAll(c));
+    }
+
+    @Test
+    void test5(){
+        HashSet<Integer> hs = new HashSet<>(List.of(6,7,8));
+        Collection<Integer> c = Collections.unmodifiableCollection(List.of(1,2,3,4,5));
+        assertFalse(hs.removeAll(c));
+    }
+
+    @Test
+    void test6(){
+        HashSet<Integer> hs = new HashSet<>(List.of(1,2,3,4));
+        Collection<Integer> c = Collections.unmodifiableCollection(List.of(5,6,7));
         assertFalse(hs.removeAll(c));
     }
 
